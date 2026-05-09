5d5d24ca0e8412d823d7bf633b0eb81271ae1861
Jayesh Kathale <jayeshkathale24@gmail.com>
2026-05-09 20:28:46 +0530

    feat: add hiero-did-sdk facade crate
    add top-level hiero-did-sdk crate that re-exports all workspace crates
    allows users to depend on a single crate instead of individual ones
    
    Signed-off-by: Jayesh Kathale <jayeshkathale24@gmail.com>

---
+++ sdk/src/lib.rs
@@ -0,0 +1,7 @@
+pub use hiero_did_core as core;
+pub use hiero_did_hcs as hcs;
+pub use hiero_did_messages as messages;
+pub use hiero_did_method as method;
+pub use hiero_did_registrar as registrar;
+pub use hiero_did_resolver as resolver;
+pub use hiero_did_signer as signer;
